ec16b8affc1b1639c4ccfff956c65e907ea280ae
Hugo Firth <hugo.firth@gmail.com>
2015-09-10 17:11:08 +1000

    updated readme

--- install.sh
+++ install.sh
@@ -4,8 +4,14 @@ set -e
 
 mkdir -p ~/bins
 
-curl https://github.com/firthh/dev-envs/raw/master/lein/leind > ~/bins/leind
-chmod +x ~/bins/leind
+cd ~/bins
+curl https://github.com/firthh/dev-envs/raw/master/lein/leind > leind
+chmod +x leind
+curl https://github.com/firthh/dev-envs/raw/master/postgres/start-postgres > start-postgres
+chmod +x start-postgres
+curl https://github.com/firthh/dev-envs/raw/master/postgres/stop-postgres > stop-postgres
+chmod +x stop-postgres
+
 
 echo "------------------------------------------------------"
 echo "                Install finished"
