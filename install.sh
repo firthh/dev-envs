88e90fcd31f6e0bd6983a244c2a04efebf4b3181
Hugo Firth <hugo.firth@gmail.com>
2015-09-10 17:09:00 +1000

    adding scripts to start and stop dev psql server

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ curl https://github.com/firthh/dev-envs/raw/master/lein/leind > ~/bins/leind
 chmod +x ~/bins/leind
 
 echo "------------------------------------------------------"
-echo "        Install finished"
+echo "                Install finished"
 echo " "
 echo "Please add the following line to ~/.bashrc (or .zshrc)"
 echo "export PATH=\"\$PATH:$(cd ~ && pwd)/bins/\""
