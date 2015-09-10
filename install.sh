0ff6ab30fbe80062e74a044fcb20bf63f8187197
Hugo Firth <hugo.firth@gmail.com>
2015-09-10 16:49:53 +1000

    added install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p ~/bins
+
+curl https://github.com/firthh/dev-envs/raw/master/lein/leind > ~/bins/leind
+chmod +x ~/bins/leind
+
+echo "------------------------------------------------------"
+echo "        Install finished"
+echo " "
+echo "Please add the following line to ~/.bashrc (or .zshrc)"
+echo "export PATH=\"\$PATH:$(cd ~ && pwd)/bins/\""
