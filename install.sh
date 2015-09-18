04ad6e31fe1e0aa98c532e051db38f5c53461428
Hugo Firth <hugo.firth@gmail.com>
2015-09-18 16:54:20 +1000

    moved everything into the parent directory to make it easier to install

--- install.sh
+++ install.sh
@@ -2,19 +2,13 @@
 
 set -e
 
-mkdir -p ~/bins
+mkdir -p ~/dev-envs
 
-cd ~/bins
-curl https://github.com/firthh/dev-envs/raw/master/lein/leind > leind
-chmod +x leind
-curl https://github.com/firthh/dev-envs/raw/master/postgres/start-postgres > start-postgres
-chmod +x start-postgres
-curl https://github.com/firthh/dev-envs/raw/master/postgres/stop-postgres > stop-postgres
-chmod +x stop-postgres
+git clone https://github.com/firthh/dev-envs.git ~/dev-envs
 
 
 echo "------------------------------------------------------"
 echo "                Install finished"
 echo " "
 echo "Please add the following line to ~/.bashrc (or .zshrc)"
-echo "export PATH=\"\$PATH:$(cd ~ && pwd)/bins/\""
+echo "export PATH=\"\$PATH:$(cd ~ && pwd)/dev-envs/\""
