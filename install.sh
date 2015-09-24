dc08526e0166baeddc49453e977dbb3a1c6f4aa8
Hugo Firth <hugo.firth@gmail.com>
2015-09-24 13:53:19 +1000

    updated install script to install from directories and ask what you want to install

--- install.sh
+++ install.sh
@@ -3,9 +3,23 @@
 set -e
 
 mkdir -p ~/dev-envs
+mkdir -p ~/dev-envs-temp
 
-git clone https://github.com/firthh/dev-envs.git ~/dev-envs
+#git clone https://github.com/firthh/dev-envs.git ~/dev-envs-temp
+#cd ~/dev-envs-temp
 
+for file in $(ls -d */)
+do
+    echo "Would you like to install $file [Y/n]"
+    read ynae
+    if [ "$ynae" == "Y" ]
+    then
+        echo "Installing $file"
+        cp $file/* ~/dev-envs
+    fi
+done
+
+rm -rf ~/dev-envs-temp
 
 echo "------------------------------------------------------"
 echo "                Install finished"
